e810aa4b00cef168022dfbaa89fb9fd0f83fb1ea
KentFujii <kent.where.the.light.is@gmail.com>
2020-05-04 17:22:54 +0900

    wip: client

---
+++ clients/twitter_client.py
@@ -0,0 +1,11 @@
+import requests
+
+class TwitterClient(object):
+    def __init__(self):
+        pass
+
+    def fetch(self)
+        pass
+
+    def load(self)
+        pass
---
+++ clients/twitter_client_test.py
@@ -0,0 +1,12 @@
+import pytest
+
+class TestTwitterClient(object):
+    @classmethod
+    def setup_class(cls):
+        self.client = TwitterClient()
+
+    def test_fetch(self):
+        pass
+
+    def test_load(self):
+        pass
