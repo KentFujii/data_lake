e586adebfeec703cd13bd1cb73f02e48450c912f
KentFujii <kent.where.the.light.is@gmail.com>
2020-05-07 00:59:36 +0900

    wip: client

--- clients/twitter_client.py
+++ clients/twitter_client.py
@@ -1,12 +1,13 @@
 import os
 import requests
+from requests.auth import HTTPBasicAuth
 import urllib.parse
 
 class TwitterClient(object):
     def __init__(self):
         TWITTER_ACCESS_KEY = os.getenv("TWITTER_ACCESS_KEY")
         TWITTER_SECRET_KEY = os.getenv("TWITTER_SECRET_KEY")
-        auth = (TWITTER_ACCESS_KEY, TWITTER_SECRET_KEY)
+        auth = HTTPBasicAuth(TWITTER_ACCESS_KEY, TWITTER_SECRET_KEY)
         data = {"grant_type": "client_credentials"}
         bearer = requests.post('https://api.twitter.com/oauth2/token', auth=auth, data=data).json()
         self.token = bearer['access_token']
