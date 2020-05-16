0dbe3ac6c1d53f54aa6cde1eedbf86040afe21a9
KentFujii <kent.where.the.light.is@gmail.com>
2020-05-16 18:44:58 +0900

    wip: k8s

--- scripts/k8s.sh
+++ scripts/k8s.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
 cd `dirname $0`/k8s
-kubectl apply -f ./
-# kubectl delete -f ./
+
+# https://github.com/nigelpoulton/TheK8sBook
+# https://kubernetes.io/docs/concepts/configuration/configmap/
+
+
+# kubectl create configmap env --dry-run=true --output=yaml
+# kubectl apply -f ./env.yml
+# kubectl delete -f ./env.yml
+
+# postgres
+kubectl apply -f ./postgres/deployment.yml
+kubectl delete -f ./postgres/deployment.yml
